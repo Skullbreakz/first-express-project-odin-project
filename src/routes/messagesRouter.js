804bdaf56f49a08c3274fc1df7a57f5bf7263894
Hamza-HM <mohzilla8@gmail.com>
2024-09-28 13:58:02 +0100

    refactored validators

--- src/routes/messagesRouter.js
+++ src/routes/messagesRouter.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const homeRouter = Router();
 const messagesController = require("../controllers/messagesController");
 const { body } = require("express-validator");
+const { messageValidator } = require("../validators/messageValidators");
 // better than random function to avoid collisions
 
 homeRouter.get("/", messagesController.getHomePage);
@@ -10,25 +11,6 @@ homeRouter.get("/msg/:id", messagesController.getMessageById);
 
 homeRouter.get("/create", messagesController.getMessagesForm);
 
-homeRouter.post(
-  "/create",
-  [
-    body("user")
-      .trim()
-      .escape()
-      .notEmpty()
-      .withMessage("user name is required")
-      .isLength({ min: 2 })
-      .withMessage("user name must be at least 2 characters long"),
-    body("text")
-      .trim()
-      .escape()
-      .notEmpty()
-      .withMessage("Message is required")
-      .isLength({ min: 2 })
-      .withMessage("Message must be at least 2 characters long"),
-  ],
-  messagesController.createMessage
-);
+homeRouter.post("/create", messageValidator, messagesController.createMessage);
 
 module.exports = homeRouter;
---
+++ src/validators/messageValidators.js
@@ -0,0 +1,33 @@
+const { body } = require("express-validator");
+
+exports.messageValidator = [
+  body("user")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("user name is required")
+    .isLength({ min: 2 })
+    .withMessage("user name must be at least 2 characters long"),
+  body("text")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Message is required")
+    .isLength({ min: 2 })
+    .withMessage("Message must be at least 2 characters long"),
+];
+
+const validateUser = [
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`First name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Last name ${lengthErr}`),
+];
